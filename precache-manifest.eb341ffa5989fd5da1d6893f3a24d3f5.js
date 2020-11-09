c8b8b80a2f9cd858b2055ec444eaa0a7c6a183f8
sidaaryan7 <sidaaryan7@gmail.com>
2020-11-09 18:58:29 +0530

    Updates

---
+++ precache-manifest.eb341ffa5989fd5da1d6893f3a24d3f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4405f9062185dfb8f40c5973264e933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab7dc27cff5d16d36027",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7dc27cff5d16d36027",
+    "url": "/static/js/main.82dfd570.chunk.js"
+  },
+  {
+    "revision": "10545e6b10910a0b9f12",
+    "url": "/static/js/runtime-main.025be44c.js"
+  }
+]);
